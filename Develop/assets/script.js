ac767ed83082408258c3ba67ab72a4be6c61fe25
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-16 23:08:34 -0700

    Completed: scheduler in JS, readme, updated with comments

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,13 +1,13 @@
 var dateEl = $('#currentDay');
 var textEl = $('textarea');
-
-var buttonEls = $('button')
+var storedSchedule = JSON.parse(window.localStorage.getItem("storedSchedule")) || [];
+var buttonEls = $('button');
+var successEl = $('#success');
 
 // Configures the current day element to display current date and time
 var currentTime = moment().format("MM/DD/YYYY hh:mm a");
 dateEl.text(currentTime);
 
-console.log(buttonEls)
 
 // Function to update the time indicator on the page
 function updateTime() {
@@ -30,51 +30,80 @@ function updateTime() {
 // Function to determine if a row is before, after, or is the current hour
 function hourCheck() {
     for (let i = 0; i < textEl.length; i++) {
-        var scheduleArea = textEl[i]
-        var rowTime = scheduleArea.dataset.time
-        var momentTime = moment(rowTime, "h a").hour()
-        var currentHour = moment().hour()
+        // Initialize Variables
+        var scheduleArea = textEl[i];
+        var rowTime = scheduleArea.dataset.time;
+        var momentTime = moment(rowTime, "h a").hour();
+        var currentHour = moment().hour();
 
+        // If else if else to check each row if it is present, past, or future
         if (momentTime == currentHour) {
-            console.log('samehour')
-            scheduleArea.setAttribute("class", "present")
+            scheduleArea.setAttribute("class", "present");
         } else if (moment(momentTime).isBefore(currentHour)) {
-            scheduleArea.setAttribute("class", "past")
-
+            scheduleArea.setAttribute("class", "past");
         } else {
-            scheduleArea.setAttribute("class", "future")
+            scheduleArea.setAttribute("class", "future");
+        };
 
-        }
+    };
 
-    }
+};
 
-}
-
+// saveContent function saves each schedule line item into specific index positions corresponding to each hour
 function saveContent(event) {
     event.preventDefault();
+    // Query selector to target the text area corresponding to the button press
     var content = event.target.closest('form').querySelector('textarea');
+
+
+    // Pulls information from webpage content
     var timeValue = content.dataset.time;
     var textValue = content.value;
-    
+    var indexValue = content.dataset.index;
 
-    console.log(content.dataset.time)
-    var storedSchedule = {
-        hour : timeValue,
+    // Populates object with updated information
+    var updatedHour = {
+        hour: timeValue,
         text: textValue,
     };
-    console.log(storedSchedule);
 
-    localStorage.setItem("schedule", JSON.stringify(storedSchedule));
+    // Pushes updated information to storedSchedule array
+    storedSchedule[indexValue] = updatedHour;
 
+    // Saves updated schedule to local storage
+    window.localStorage.setItem("storedSchedule", JSON.stringify(storedSchedule));
 
+    successEl.text(`Successfully submitted for ${timeValue}`).delay(2000).fadeOut(600, resetSuccess);
+
+};
+
+// Renders the saved schedule; used for on-page load
+function renderSavedSchedule() {
+    for (let i = 0; i < storedSchedule.length; i++) {
+
+        if (storedSchedule[i] != null) {
+            textEl[i].textContent = storedSchedule[i].text;
+        } else {
+            textEl[i].textContent = "";
+        };
+    };
+};
+
+// Resets the success notification
+function resetSuccess() {
+    successEl.text("");
+    successEl.removeAttr("style");
 
 }
 
 
+// Initial execution of function to enable formatting on webpage load
+hourCheck();
+renderSavedSchedule();
+
+// setInterval function to update time
+setInterval(updateTime, 1000);
 
 
-hourCheck()
-
-setInterval(updateTime, 1000)
-
-buttonEls.on("click",saveContent)
+// Event listeners
+buttonEls.on("click", saveContent);
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Work Day Scheduler Starter Code
+
+The project can be found at the following link: https://totoero.github.io/Workday_Scheduler/
+
+
+
+This workday scheduler is developed for the user to be able to plan out individual hours of their day. By entering their plans in each hour row, they are able to save their plans using the icons on the right side. An indicator will pop to to show that it was successfuly saved to local storage. If a user refreshes the page, the text information will still exist within the fields they saved. Each row dynamically updates as hours pass to indicate if the specific hour row is in the past, present, or future.
+
+The code works by utilizing form elements in the HTML to submit data, and process it within Javascript to save the data into the browser's local storage. 
+
+![A user inputs their schedule for the day for 8AM, 10AM, and 4PM. They save the input, mousing over the notification that appears to indicate that the plans have been successfully submitted. Afterwards, the page is refreshed to show the information is still present on the page.](./assets/work_day_scheduler.gif)
